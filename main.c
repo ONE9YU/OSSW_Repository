2e779c08e19856fb113436ece3d3794ff2958312
won9kyu <wonkyu3849@naver.com>
2023-06-17 21:53:42 +0900

    makeTitle

--- main.c
+++ main.c
@@ -2,19 +2,10 @@
 #include <windows.h>
 #include <time.h>
 #include <conio.h>
-//#include "title.h"
-#include <math.h>
-
-#define COL				GetStdHandle(STD_OUTPUT_HANDLE)
-#define RED				SetConsoleTextAttribute(COL, 0x000c);
+#include "title.h"
 
 #define GBOARD_WIDTH 50
 #define GBOARD_HEIGHT 30
-#define GREEN			SetConsoleTextAttribute(COL, 0x0002);
-#define BLUE_GREEN		SetConsoleTextAttribute(COL, 0x0003);
-#define LIGHT_WHITE		SetConsoleTextAttribute(COL, 0x000f);
-#define YELLOW			SetConsoleTextAttribute(COL, 0x000e);
-#define DARK_YELLOW		SetConsoleTextAttribute(COL, 0x0006);
 
 #define ONE 49
 #define TWO 50
@@ -34,46 +25,12 @@ int mode;
 int next = 0;
 int curTime;
 
-int v_num = 0;
-int vertical_num = 0;
-
 typedef struct Virus {
 	int x;
 	int y;
 }Virus;
 
-typedef struct VirusOneby {
-	int x;
-	int y;
-}VirusOneby;
-
-typedef struct VirusVertical {
-	int x;
-	int y;
-}VirusVertical;
-
-typedef struct VirusCircle {
-	int x;
-	int y;
-}VirusCircle;
-
-typedef struct VirusSquare {
-	int x;
-	int y;
-}VirusSquare;
-
 Virus* virus;
-VirusOneby virusOneby[100];
-VirusVertical virusVertical[GBOARD_HEIGHT];
-VirusCircle virusCircle[100];
-VirusSquare virusSquare[200];
-
-struct game_util {
-	int score;
-	int life;
-};
-
-struct game_util game_util;
 
 void SetCurrentCursorPos(int x, int y)
 {
@@ -201,7 +158,6 @@ void drawGameBoard() {
 	curPosY = 0;
 	SetCurrentCursorPos(GBOARD_WIDTH, 0);
 }
-/*
 //"SELECT MODE" 출력
 void printTitle2() {
 	int x = 0, y = 4;
@@ -211,9 +167,8 @@ void printTitle2() {
 			x = (k * 4) * 2 + i * 2 + 27;
 			for (int j = 0; j < 5; j++) {
 				SetCurrentCursorPos(x, y + j);
-				if (title2[k][j][i] == 1) {
-					YELLOW printf("■");
-				}
+				if (title2[k][j][i] == 1)
+					printf("■");
 				else
 					printf("  ");
 			}
@@ -230,7 +185,6 @@ void printTitle2() {
 				SetCurrentCursorPos(x, y + j);
 				if (title3[k][j][i] == 1)
 					printf("■");
-
 				else
 					printf("  ");
 			}
@@ -261,7 +215,7 @@ void printTitle1() {
 	SetCurrentCursorPos(38, 20);
 	printf("Press Any Key to Start");
 }
-*/
+
 void removeCursor(void)
 {
 	CONSOLE_CURSOR_INFO curInfo;
@@ -272,18 +226,18 @@ void removeCursor(void)
 //모드 선택
 void selectMode() {
 	int key;
-	//printTitle1();
+	printTitle1();
 
 	while (1) {
 		if (_kbhit() != 0) {
 			key = _getch();
 			redrawGameBoard();
-			//printTitle2();
+			printTitle2();
 			break;
 		}
 	}
-	/*
-	//cassic mode button
+
+	//classic mode button
 	drawModeButton(5);
 	SetCurrentCursorPos(10, 21);
 	printf("Classic");
@@ -313,13 +267,13 @@ void selectMode() {
 	printf("Final");
 	SetCurrentCursorPos(84, 23);
 	printf("Mode");
-	*/
+
 	curPosX = GBOARD_WIDTH;
 	curPosY = 0;
 	SetCurrentCursorPos(GBOARD_WIDTH, 0);
 
 	//모드 선택하는 입력 키 받기
-	//modeKeyInput();
+	modeKeyInput();
 }
 //인간 생성
 void createHuman() {
@@ -332,207 +286,52 @@ void createHuman() {
 //바이러스 생성
 void createVirus() {
 
-	virus = (Virus*)malloc(5 * sizeof(Virus));
+	virus = (Virus*)malloc(100 * sizeof(Virus));
 
 	srand((unsigned int)time(NULL));
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 100; i++) {
 		virus[i].x = rand() % 97 + 1;
 		virus[i].y = rand() % 28 + 1;
 		SetCurrentCursorPos(virus[i].x, virus[i].y);
 		printf("*");
 	}
 }
-
-void createVirusOneby() {
-
-
-
-	srand((unsigned int)time(NULL));
-
-	virusOneby[v_num].x = rand() % 97 + 1;
-	virusOneby[v_num].y = rand() % 28 + 1;
-
-	SetCurrentCursorPos(virusOneby[v_num].x, virusOneby[v_num].y);
-	printf("*");
-
-	v_num++;
-
-}
-
-void createVirusVertical() {
-	int i;
-
-	for (i = 0; i < GBOARD_HEIGHT - 2; i++) {
-		virusVertical[i].x = 2;
-		virusVertical[i].y = 1 + i;
-
-		SetCurrentCursorPos(virusVertical[i].x, virusVertical[i].y);
-		printf("*");
-
-	}
-
-
-}
-
-/*
-void createVirusCircle() {
-	int k = 0;
-	virusCircle[0].x = humanCurPosX;
-	virusCircle[0].y = humanCurPosY + 10;
-	virusCircle[1].x = humanCurPosX + 2;
-	virusCircle[1].y = humanCurPosY + 10;
-	virusCircle[2].x = humanCurPosX - 2;
-	virusCircle[2].y = humanCurPosY + 10; //6
-	virusCircle[3].x = humanCurPosX + 10;
-	virusCircle[3].y = humanCurPosY + 9;
-	virusCircle[4].x = humanCurPosX - 10;
-	virusCircle[4].y = humanCurPosY + 9; //5
-	virusCircle[5].x = humanCurPosX + 18;
-	virusCircle[5].y = humanCurPosY;
-	virusCircle[6].x = humanCurPosX - 18;
-	virusCircle[6].y = humanCurPosY; //4
-	virusCircle[7].x = humanCurPosX;
-	virusCircle[7].y = humanCurPosY - 10;
-	virusCircle[8].x = humanCurPosX;
-	virusCircle[8].y = humanCurPosY - 10; //3
-	virusCircle[9].x = humanCurPosX + 4;
-	virusCircle[9].y = humanCurPosY + 2;
-	virusCircle[10].x = humanCurPosX - 4;
-	virusCircle[10].y = humanCurPosY + 2; //2
-	virusCircle[11].x = humanCurPosX + 4;
-	virusCircle[11].y = humanCurPosY + 1;
-	virusCircle[12].x = humanCurPosX - 4;
-	virusCircle[12].y = humanCurPosY + 1; //1
-	virusCircle[13].x = humanCurPosX + 4;
-	virusCircle[13].y = humanCurPosY + 0;
-	virusCircle[14].x = humanCurPosX - 4;
-	virusCircle[14].y = humanCurPosY + 0; //0
-	for (int i = 0; i <= 8; i++) {
-		SetCurrentCursorPos(virusCircle[i].x, virusCircle[i].y);
-		printf("*");
-	}
-}
-*/
-void createVirusSquare() {
-
-	int i, k = 0;
-
-	for (i = 0; i < 18; i++) {
-		virusSquare[k].x = humanCurPosX + i;
-		virusSquare[k].y = humanCurPosY - 10;
-		k++;
-
-	}
-
-	for (i = 0; i < 18; i++) {
-		virusSquare[k].x = humanCurPosX - i;
-		virusSquare[k].y = humanCurPosY - 10;
-		k++;
-
-	}
-
-	for (i = 0; i < 18; i++) {
-		virusSquare[k].x = humanCurPosX + i;
-		virusSquare[k].y = humanCurPosY + 10;
-		k++;
-
-	}
-
-	for (i = 0; i < 18; i++) {
-		virusSquare[k].x = humanCurPosX - i;
-		virusSquare[k].y = humanCurPosY + 10;
-		k++;
-
-	}
-
-	for (i = 0; i < 10; i++) {
-		virusSquare[k].x = humanCurPosX + 18;
-		virusSquare[k].y = humanCurPosY + i;
-		k++;
-
-	}
-
-	for (i = 0; i < 10; i++) {
-		virusSquare[k].x = humanCurPosX - 18;
-		virusSquare[k].y = humanCurPosY + i;
-		k++;
-
-	}
-
-	for (i = 0; i < 10; i++) {
-		virusSquare[k].x = humanCurPosX + 18;
-		virusSquare[k].y = humanCurPosY - i;
-		k++;
-
-	}
-
-	for (i = 0; i < 10; i++) {
-		virusSquare[k].x = humanCurPosX - 18;
-		virusSquare[k].y = humanCurPosY - i;
-		k++;
-
-	}
-
-	for (int i = 0; i < 112; i++) {
-		SetCurrentCursorPos(virusSquare[i].x, virusSquare[i].y);
-		printf("*");
-
-	}
-
-}
-
-
 //충돌 검사
-int DetectCollision() {
-	int i, j;
+int DetectCollision(int posX, int posY) {
 
-	for (i = 0; i < 10; i++) {
-		if (virus[i].x == humanCurPosX && virus[i].y == humanCurPosY) {
-			return 2;
-		}
-	}
-	for (i = 0; i < v_num; i++) {
-		if (virusOneby[i].x == humanCurPosX && virusOneby[i].y == humanCurPosY) {
-			return 2;
-		}
-	}
-
+	if (posX == 1 || posX == GBOARD_WIDTH * 2 - 2 || posY == 0 || posY == GBOARD_HEIGHT - 1)
+		return 0;
 
 	return 1;
-	//if (posX == 1 || posX == GBOARD_WIDTH * 2 - 2 || posY == 0 || posY == GBOARD_HEIGHT - 1)
-	//	return 0;
-
-	//return 1;
 }
 
 void ShiftRight(void) {
 
-	//	if (!DetectCollision(humanCurPosX + 1, humanCurPosY))
-	//		return ;
+	if (!DetectCollision(humanCurPosX + 1, humanCurPosY))
+		return;
 	SetCurrentCursorPos(humanCurPosX, humanCurPosY);
 	printf(" ");
-	humanCurPosX += 2;
+	humanCurPosX += 1;
 	SetCurrentCursorPos(humanCurPosX, humanCurPosY);
 	printf("@");
-
 }
 
 void ShiftLeft(void) {
 
-	//	if (!DetectCollision(humanCurPosX - 1, humanCurPosY))
-	//		return ;
+	if (!DetectCollision(humanCurPosX - 1, humanCurPosY))
+		return;
 	SetCurrentCursorPos(humanCurPosX, humanCurPosY);
 	printf(" ");
-	humanCurPosX -= 2;
+	humanCurPosX -= 1;
 	SetCurrentCursorPos(humanCurPosX, humanCurPosY);
 	printf("@");
 }
 
 void ShiftUp(void) {
 
-	//	if (!DetectCollision(humanCurPosX, humanCurPosY - 1))
-	//		return ;
+	if (!DetectCollision(humanCurPosX, humanCurPosY - 1))
+		return;
 	SetCurrentCursorPos(humanCurPosX, humanCurPosY);
 	printf(" ");
 	humanCurPosY -= 1;
@@ -542,8 +341,8 @@ void ShiftUp(void) {
 
 void ShiftDown(void) {
 
-	//	if (!DetectCollision(humanCurPosX, humanCurPosY + 1))
-	//		return ;
+	if (!DetectCollision(humanCurPosX, humanCurPosY + 1))
+		return;
 
 	SetCurrentCursorPos(humanCurPosX, humanCurPosY);
 	printf(" ");
@@ -554,7 +353,7 @@ void ShiftDown(void) {
 //추적 바이러스
 void trackingVirus() {
 
-	for (int i = 0; i < 5; i++) {
+	for (int i = 0; i < 100; i++) {
 		SetCurrentCursorPos(virus[i].x, virus[i].y);
 		printf(" ");
 
@@ -596,74 +395,7 @@ void trackingVirus() {
 		SetCurrentCursorPos(virus[i].x, virus[i].y);
 		printf("*");
 	}
-
-
 }
-
-void trackingVirusOneby() {
-
-	for (int i = 0; i < v_num; i++) {
-		SetCurrentCursorPos(virusOneby[i].x, virusOneby[i].y);
-		printf(" ");
-
-		int x, y;
-		x = virusOneby[i].x - humanCurPosX;
-		y = virusOneby[i].y - humanCurPosY;
-
-		if (x == 0 && y == 0)
-			continue;
-		else if (x == 0) {
-			if (y > 0)
-				virusOneby[i].y--;
-			else
-				virusOneby[i].y++;
-		}
-		else if (y == 0) {
-			if (x > 0)
-				virusOneby[i].x--;
-			else
-				virusOneby[i].x++;
-		}
-		else if (x > 0 && y > 0) {
-			virusOneby[i].x--;
-			virusOneby[i].y--;
-		}
-		else if (x > 0 && y < 0) {
-			virusOneby[i].x--;
-			virusOneby[i].y++;
-		}
-		else if (x < 0 && y > 0) {
-			virusOneby[i].x++;
-			virusOneby[i].y--;
-		}
-		else {
-			virusOneby[i].x++;
-			virusOneby[i].y++;
-		}
-
-		SetCurrentCursorPos(virusOneby[i].x, virusOneby[i].y);
-		printf("*");
-
-
-	}
-
-	vertical_num++;
-}
-void trackingVirusVertical() {
-
-	for (int i = 0; i < GBOARD_HEIGHT - 2; i++) {
-
-		SetCurrentCursorPos(virusVertical[i].x, virusVertical[i].y);
-		printf(" ");
-
-		virusVertical[i].x++;
-
-		SetCurrentCursorPos(virusVertical[i].x, virusVertical[i].y);
-		printf("*");
-
-	}
-}
-
 //인간 이동
 void moveHuman() {
 	int key;
@@ -686,19 +418,13 @@ void moveHuman() {
 				break;
 			}
 		}
+		Sleep(10);
+
 	}
-
-
-
+	trackingVirus();
 }
 
 int main() {
-	int i, j, num_cnt = 0;
-	game_util.life = 5;
-	int check = 0;
-	int vertical_cnt = 0;
-	int circle_cnt = 0;
-
 	//system("mode con:cols=100 lines=30");   // cols: 가로, lines: 세로
 	removeCursor();
 	drawGameBoard();
@@ -706,69 +432,8 @@ int main() {
 
 	createHuman();
 	createVirus();
-	for (i = 1;; i++) {
-
-		if (_kbhit())
-			moveHuman();
-
-		Sleep(100);
-
-		if (_kbhit())
-			moveHuman();
-
-		if (i % 100 == 0)
-		{
-			num_cnt++;
-		}
-
-		if (_kbhit())
-			moveHuman();
-
-		for (j = 0; j <= num_cnt; j++)
-		{
-			createVirusOneby();
-		}
-
-		if (_kbhit())
-			moveHuman();
-
-		trackingVirus();
-
-		if (_kbhit())
-			moveHuman();
-
-		trackingVirusOneby();
-
-		if (_kbhit())
-			moveHuman();
-
-		check = DetectCollision();
-		if (check == 2) {
-			SetCurrentCursorPos(humanCurPosX, humanCurPosY);
-			game_util.life = game_util.life - 1;
-			RED printf("∩\a");
-			Sleep(10000);
-		}
-
-		if (_kbhit())
-			moveHuman();
-
-		if (vertical_num == 10) {
-			createVirusVertical();
-			//createVirusCircle();
-			//createVirusSquare();
-			vertical_cnt++;
-		}
-
-		if (vertical_cnt >= 1) {
-			trackingVirusVertical();
-		}
-
-		Sleep(100);
-
-		if (_kbhit())
-			moveHuman();
-
+	while (1) {
+		moveHuman();
 	}
 	getchar();
 	return 0;
